ba7033fc059f8695fc00124052b2b0fbf25749bd
SantiFerretto <131539630+SantiFerretto@users.noreply.github.com>
2023-10-31 18:18:40 -0300

    Add files via upload

--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -10,36 +10,36 @@ function Trending (){
         <section className="trending-section">
         <a href="/papa" style={{width:"25%", height:"auto", textDecoration:"none"}}>
                         <div style={{ height:"12rem",overflow:"hidden"}}>
-                            <img className="img" src="img.png" style={{height:"100%"}}/>
+                            <img className="img" src="jp_funcionalidad.png" style={{height:"100%"}}/>
                         </div>
                         <div className='card-info'>
                             <img src="in.png"/>
                             <div className='card-detail'>
-                                <h3>Fluasasastso</h3>
+                                <h3>JimPal</h3>
                                 <p>Available for Work</p>
                             </div>
                         </div>
                     </a>
             <a href="/papa" style={{width:"25%", height:"auto", textDecoration:"none"}}>
                         <div style={{ height:"12rem",overflow:"hidden"}}>
-                            <img className="img" src="img.png" style={{height:"100%"}}/>
+                            <img className="img" src="braiin_funcionalidad.png" style={{height:"100%"}}/>
                         </div>
                         <div className='card-info'>
                             <img src="in.png"/>
                             <div className='card-detail'>
-                                <h3>Fluasasastso</h3>
+                                <h3>Braiin</h3>
                                 <p>Available for Work</p>
                             </div>
                         </div>
                     </a>
             <a href="/papa" style={{width:"25%", height:"auto", textDecoration:"none"}}>
                         <div style={{ height:"12rem",overflow:"hidden"}}>
-                            <img className="img" src="img.png" style={{height:"100%"}}/>
+                            <img className="img" src="random_bites_funcionalidad.png" style={{height:"100%"}}/>
                         </div>
                         <div className='card-info'>
                             <img src="in.png"/>
                             <div className='card-detail'>
-                                <h3>Fluasasastso</h3>
+                                <h3>Random Bites</h3>
                                 <p>Available for Work</p>
                             </div>
                         </div>
