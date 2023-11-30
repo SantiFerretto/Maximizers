80bb72b7f198682049ff1d13a66f92b59772aeca
SantiFerretto <131539630+SantiFerretto@users.noreply.github.com>
2023-11-29 23:46:09 -0300

    Add files via upload

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >
+      <body style = "background-color #448480" >
         <nav style={{display:"flex", justifyContent:"space-between", padding:"1em 1em", alignItems:"center", maxWidth:"1300px", margin:"0 auto", marginBottom:"5rem"}}>
           <div style={{height:"4rem"}}>
             <img src="maximizers-logo-png.png" style={{height:"90%"}} />
