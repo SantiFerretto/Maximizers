d2acf406d1847cde4836143734be75d4ebf25417
SantiFerretto <131539630+SantiFerretto@users.noreply.github.com>
2023-12-02 11:02:32 -0300

    Add files via upload

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,13 +1,13 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body style={{backgroundColor:"#448480"}}>
         <nav style={{display:"flex", justifyContent:"space-between", padding:"1em 1em", alignItems:"center", maxWidth:"1300px", margin:"0 auto", marginBottom:"5rem"}}>
           <div style={{height:"4rem"}}>
             <img src="maximizers-logo-png.png" style={{height:"90%"}} />
           </div>
-          <div style={{padding:"0.75rem", backgroundColor:"#fe912a"}}>
-            <a href="https://forms.gle/EqirDqrE2Pcb62Vk7" style={{height:"1.5em", backgroundColor:"#fe912a", fontWeight:"900", border:"none"}}>
+          <div style={{padding:"0.75rem", backgroundColor:"#fe912a", borderRadius:"10px"}}>
+            <a href="https://forms.gle/EqirDqrE2Pcb62Vk7" style={{color:"black", textDecoration: "none", height:"1.5em", fontWeight:"900", border:"none"}}>
               PROMOCIONATE
             </a>
           </div>
